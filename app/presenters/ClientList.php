29f5563f02a00f0d43d30c6f234f0d79d75afa08
abr011 <alesbrom@ales-Air-2.local>
2017-10-06 16:51:22 +0200

    Client list - db connected
    Ha!

--- app/presenters/ClientList.php
+++ app/presenters/ClientList.php
@@ -3,12 +3,24 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use Nette;
 
 class ClientListPresenter extends BasePresenter
 {
+
+    private $database;
+
 	public function renderDefault()
 	{
-
+        $this->template->items = $this->database->table('client');
 	}
 
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
 }
+
+
+
